8cf67bee0595ca8c490aa0d05a5618826a611ad5
mmongan <marty.mongan@gmail.com>
2026-04-04 11:31:36 -0500

    Use inverted RGB as Opacity Texture to properly mask black notes on transparent planes

--- src/main.ts
+++ src/main.ts
@@ -378,24 +378,28 @@ class App {
                     const ctx = sliceTexture.getContext() as CanvasRenderingContext2D;
 
                     // Ensure clear background before drawing
-                    ctx.clearRect(0, 0, slicePixW, totalPixelsH);
-                    ctx.drawImage(osmdCanvas, srcX, 0, slicePixW, totalPixelsH, 0, 0, slicePixW, totalPixelsH);
-                      const imgData = ctx.getImageData(0, 0, slicePixW, totalPixelsH);
-                      const data = imgData.data;
-                      for (let k = 0; k < data.length; k += 4) {
-                          if (data[k] >= 200 && data[k+1] >= 200 && data[k+2] >= 200) {
-                              data[k] = 0; data[k+1] = 0; data[k+2] = 0; data[k+3] = 0;
-                          } else {
-                              const brightness = (data[k] + data[k+1] + data[k+2]) / 3;
-                              data[k] = 0;
-                              data[k+1] = 0;
-                              data[k+2] = 0;
-                              data[k+3] = 255 - brightness;
-                          }
-                      }
-                      ctx.putImageData(imgData, 0, 0);
-                    // Update the texture
-                    sliceTexture.update();
+                      ctx.clearRect(0, 0, slicePixW, totalPixelsH);
+                      ctx.drawImage(osmdCanvas, srcX, 0, slicePixW, totalPixelsH, 0, 0, slicePixW, totalPixelsH);
+                        const imgData = ctx.getImageData(0, 0, slicePixW, totalPixelsH);
+                        const data = imgData.data;
+                        for (let k = 0; k < data.length; k += 4) {
+                            const brightness = (data[k] + data[k+1] + data[k+2]) / 3;
+                            const inverted = 255 - brightness;
+                            data[k] = inverted;
+                            data[k+1] = inverted;
+                            data[k+2] = inverted;
+                            data[k+3] = 255;
+                        }
+                        ctx.putImageData(imgData, 0, 0);
+                      // Update the texture
+                      sliceTexture.update();
+                      sliceTexture.getAlphaFromRGB = true;
+                      
+                      mat.diffuseTexture = null;
+                      mat.emissiveTexture = null;
+                      mat.opacityTexture = sliceTexture;
+                      mat.transparencyMode = 2; // ALPHABLEND
+                      mat.emissiveColor = new Color3(0, 0, 0); // Solid black color masked by opacity map
                     console.log(`  Slice ${i}: drawn ${slicePixW}x${totalPixelsH}px`);
                 }
 
